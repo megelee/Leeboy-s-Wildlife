dfa50110dadcca2a946e3290c6290ec1d3fda040
megelee <megan_baker_1992@yahoo.com>
2023-10-17 14:05:35 -0500

    Home page complete

--- client/src/components/ApplicationViews.js
+++ client/src/components/ApplicationViews.js
@@ -5,17 +5,28 @@ import Register from "./auth/Register";
 import ServiceList from "./services/ServiceList.js";
 import ServiceDetails from "./services/ServiceDetails.js";
 
+// Your description text
+const text =
+  "Leeboy's Wildlife Removal specializes in providing humane wildlife removal services. We are dedicated to safely and ethically removing wildlife, such as raccoons, squirrels, and birds, from residential and commercial properties. Our mission is to protect both your property and the well-being of the wildlife we encounter. With our experienced team, we strive to ensure a safe and harmonious coexistence between humans and wildlife.";
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
   return (
-    <Routes>
-      <Route path="/">
-        <Route path="login" element={<Login setLoggedInUser={setLoggedInUser} />} />
-        <Route path="register" element={<Register setLoggedInUser={setLoggedInUser} />} />
-        <Route path="services" element={<ServiceList />} />
-        <Route path="services/:serviceId" element={<ServiceDetails />} /> 
-      </Route>
-      <Route path="*" element={<p>Whoops, nothing here...</p>} />
-    </Routes>
+    <>
+      {/* Render your description text outside of Routes */}
+      <div className="description">
+        <p>{text}</p>
+      </div>
+
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login setLoggedInUser={setLoggedInUser} />} />
+          <Route path="register" element={<Register setLoggedInUser={setLoggedInUser} />} />
+          <Route path="services" element={<ServiceList />} />
+          <Route path="services/:serviceId" element={<ServiceDetails />} />
+        </Route>
+        <Route path="*" element={<p>Whoops, nothing here...</p>} />
+      </Routes>
+    </>
   );
 }
+
